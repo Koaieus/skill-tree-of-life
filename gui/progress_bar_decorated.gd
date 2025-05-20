849fd65607fd8969ae86e03e55ae5bc581d91d7d
Bram Hoonhout <b.hoonhout@gmail.com>
2025-05-20 19:26:28 +0200

    Update XP progress bar tooltip

--- gui/progress_bar_decorated.gd
+++ gui/progress_bar_decorated.gd
@@ -6,9 +6,18 @@ class_name DecoratedProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_update_tooltip_text()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_progress_bar_value_changed(value: float) -> void:
+	_update_tooltip_text()
+
+func _on_progress_bar_changed() -> void:
+	_update_tooltip_text()
+
+func _update_tooltip_text() -> void:
+	tooltip_text = "Next level: %d / %d" % [_bar.value, _bar.max_value]
