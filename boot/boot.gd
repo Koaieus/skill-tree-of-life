d1f0b3a5955d58f345eb76d266cda833dc94fc70
Bram Hoonhout <b.hoonhout@gmail.com>
2025-06-25 23:04:28 +0200

    Start implementing new GameRoot scene, booted by boot.gd

--- boot/boot.gd
+++ boot/boot.gd
@@ -3,4 +3,4 @@ extends Node
 
 func _ready():
 	# Load settings, apply window options, etc.
-	get_tree().change_scene("res://scenes/game_root.tscn")
+	get_tree().change_scene_to_file("res://scenes/game_root.tscn")
